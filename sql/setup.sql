9838cc37cd288b7c196bb7532a1fc1510c2383eb
THOMAS HARDING <hardingt604@gmail.com>
2022-07-14 12:37:24 -0500

    wow look its the most basic database integration possible

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS Characters CASCADE;
+-- DROP TABLE IF EXISTS Skills;
+-- DROP TABLE IF EXISTS CharacterSkills;
+
+CREATE TABLE Characters (
+    character_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    value INT NOT NULL
+);
+
+INSERT INTO Characters (value) (
+    VALUES (15), (21)
+);
