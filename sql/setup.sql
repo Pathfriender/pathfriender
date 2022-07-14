77e1216f46fd941f401505c225cfce17136d6103
Khayman King <khaymanak@gmail.com>
2022-07-14 10:41:56 -0700

    sql table for skills built out

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,33 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLES IF EXISTS skills;
+
+CREATE TABLE skills (
+  id INTEGER PRIMARY KEY,
+  skill_name TEXT,
+  skill_stat TEXT
+);
+
+INSERT INTO skills
+(skill_name, skill_stat)
+
+VALUES
+('acrobatics','dexterity'),
+('animal handling','wisdom'),
+('arcana','intelligence'),
+('athletics','strength'),
+('deception','charisma'),
+('history','intelligence'),
+('insight','wisdom'),
+('intimidation','charisma'),
+('investigation','intelligence'),
+('medicine','wisdom'),
+('nature','intelligence'),
+('perception','wisdom'),
+('performance','charisma'),
+('persuasion','charisma'),
+('religion','intelligence'),
+('sleight of hand','dexterity'),
+('stealth','dexterity'),
+('survival','wisdom');
+
