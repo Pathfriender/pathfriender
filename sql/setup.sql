0b9b3899916e46252778e2eab96b6bc9dfd7bba2
Khayman King <khaymanak@gmail.com>
2022-07-14 10:54:40 -0700

    built out sql table for characters

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,36 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLES IF EXISTS skills;
+-- Use this file to define your SQL tables
+-- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS character_skills;
+DROP TABLE IF EXISTS characters CASCADE;
 
 CREATE TABLE skills (
-  id INTEGER PRIMARY KEY,
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   skill_name TEXT,
   skill_stat TEXT
+);
+
+CREATE TABLE character_skills (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  character_id INTEGER,
+  skill_id INTEGER,
+  bonus INTEGER  
+);
+
+CREATE TABLE characters(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    character_id INTEGER,
+    character_name TEXT,
+    experience INTEGER,
+    race TEXT,
+    dexterity INT,
+    strength INT,
+    intelligence INT,
+    constitution INT,
+    wisdow INT,
+    charisma INT
 );
 
 INSERT INTO skills
