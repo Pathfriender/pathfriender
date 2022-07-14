bc0f08cd16912cbb952c2e1eebfe8c14ee0d5a6a
Khayman King <khaymanak@gmail.com>
2022-07-14 12:03:40 -0700

    updated sql tables to have inventory, items, weapons, armor

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS skills;
 DROP TABLE IF EXISTS character_skills;
 DROP TABLE IF EXISTS characters CASCADE;
+DROP TABLE IF EXISTS inventory CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS weapons CASCADE;
+DROP TABLE IF EXISTS armor CASCADE;
 
 CREATE TABLE skills (
   skills_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -16,7 +18,9 @@ CREATE TABLE character_skills (
   character_skills_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   character_id INTEGER,
   skill_id INTEGER,
-  bonus INTEGER  
+  bonus INTEGER, 
+  FOREIGN KEY (character_id) REFERENCES characters(character_id),
+  FOREIGN KEY (skill_id) REFERENCES skills(skills_id)
 );
 
 CREATE TABLE characters(
@@ -26,6 +30,8 @@ CREATE TABLE characters(
     experience INTEGER,
     class TEXT,
     race TEXT,
+    feats TEXT,
+    background TEXT,
     dexterity INT,
     strength INT,
     intelligence INT,
@@ -33,7 +39,41 @@ CREATE TABLE characters(
     wisdom INT,
     charisma INT
 );
-
+CREATE TABLE inventory (
+    inventory_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    character_id INT,
+    weapon_id BIGINT,
+    armor_id BIGINT,
+    item_id BIGINT
+    FOREIGN KEY (character_id) REFERENCES characters(character_id),
+    FOREIGN KEY (weapon_id) REFERENCES weapons(weapon_id),
+    FOREIGN KEY (armor_id) REFERENCES armors(armor_id),
+    FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
+CREATE TABLE armor (
+    armor_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    armor_name TEXT,
+    armor_type TEXT,
+    armor_ac INT,
+    armor_weight INT,
+    armor_value INT,
+    armor_description TEXT
+);
+CREATE TABLE weapons (
+    weapon_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    weapon_name TEXT,
+    weapon_type TEXT,
+    weapon_damage TEXT,
+    weapon_weight INT,
+    weapon_value INT,
+    weapon_description TEXT
+);
+CREATE TABLE items (
+    item_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    item_name TEXT,
+    item_type TEXT,
+    item_description TEXT
+);
 INSERT INTO skills
 (skill_name, skill_stat)
 
