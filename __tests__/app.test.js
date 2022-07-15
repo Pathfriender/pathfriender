4d03c88d938eb69895cf9c81314f9105435e37d5
Khayman King <khaymanak@gmail.com>
2022-07-15 10:44:25 -0700

    advantage roller working as intended

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,18 @@ const setup = require('../data/setup');
 // const request = require('supertest');
 // const app = require('../lib/app');
 const  { disadvantageRoller } = require('../lib/utils/disadvantage-roller.js');
-
+const { advantageRoller } = require('../lib/utils/advantage-roller.js');
 
 describe('pathfriender routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('rolls the dice and returns a number of 1 - x depending on the size of the dice', async() => {
+  it('disadvantage rolls the dice and returns a number of 1 - x depending on the size of the dice', async() => {
     const roll = disadvantageRoller(20, 1);
+    expect(0 < roll[0] <= 20); 
+  });
+  it('advantage rolls the dice and returns a number of 1 - x depending on the size of the dice', async() => {
+    const roll = advantageRoller(20, 1);
     expect(0 < roll[0] <= 20); 
   });
   afterAll(() => {
