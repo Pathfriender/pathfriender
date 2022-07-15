3c41432da3e80d03ee8ec1ef87721ee49473f421
Khayman King <khaymanak@gmail.com>
2022-07-15 10:42:34 -0700

    disadvantage dice roll working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,17 +1,17 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
-const diceroller = require('../lib/commands/dice-roll');
+// const request = require('supertest');
+// const app = require('../lib/app');
+const  { disadvantageRoller } = require('../lib/utils/disadvantage-roller.js');
 
 
 describe('pathfriender routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('makes a new character', async() => {
-    
-    
+  it('rolls the dice and returns a number of 1 - x depending on the size of the dice', async() => {
+    const roll = disadvantageRoller(20, 1);
+    expect(0 < roll[0] <= 20); 
   });
   afterAll(() => {
     pool.end();
