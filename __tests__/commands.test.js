e217252826bb1619bad3e8923d0d09306eabde67
Khayman King <khaymanak@gmail.com>
2022-07-18 14:08:10 -0700

    added test to retrieve stats by passing in characters name

--- __tests__/commands.test.js
+++ __tests__/commands.test.js
@@ -9,11 +9,21 @@ describe('Character routes', () => {
     return setup(pool);
   });
 
-  it('fetch a character', async () => {
+  it('fetches a character by user id and returns characters name', async () => {
     const res = await Character.getCharNameByUserId(1);
     expect(res).toBe('test');
   });  
-});
+});  
+it('Gets stats of a characters name', async () => {
+  const res = await Character.getStats('test');
+  expect(res.charisma).toBe(10);
+  expect(res.strength).toBe(10);
+  expect(res.dexterity).toBe(10);
+  expect(res.intelligence).toBe(10);
+  expect(res.constitution).toBe(10);
+  expect(res.wisdom).toBe(10);
+});  
+
 
 afterAll(() => {
   pool.end();
